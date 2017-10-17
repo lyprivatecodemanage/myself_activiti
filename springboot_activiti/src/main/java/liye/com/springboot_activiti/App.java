4ecfffb87cc2aeb51f65d0c7fa37b791122612b4
liye <471064843@qq.com>
2017-10-18 00:27:20 +0800

    liye

---
+++ springboot_activiti/src/main/java/liye/com/springboot_activiti/App.java
@@ -0,0 +1,13 @@
+package liye.com.springboot_activiti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ springboot_activiti/src/main/java/liye/com/springboot_activiti/controller/TestOne.java
@@ -0,0 +1,29 @@
+package liye.com.springboot_activiti.controller;
+
+import java.io.InputStream;
+
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.repository.Deployment;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+public class TestOne {
+	@Autowired
+	private RuntimeService runtimeService;
+	@Autowired
+	private RepositoryService  repositoryService;
+	//发布流程
+	@Test
+	public void start(){
+		InputStream inputStreamBpmn = this.getClass().getResourceAsStream("MyProcess.bpmn");
+		InputStream inputStreamPng = this.getClass().getResourceAsStream("MyProcess.png");
+		Deployment deployment = repositoryService.createDeployment()//创建一个部署对象
+				.name("流程定义")//添加部署的名称
+				.addInputStream("MyProcess.bpmn", inputStreamBpmn)
+				.addInputStream("MyProcess.png", inputStreamPng)
+				.deploy();//完成部署
+				System.out.println("部署ID："+deployment.getId());
+				System.out.println("部署名称："+deployment.getName());
+	}
+}
